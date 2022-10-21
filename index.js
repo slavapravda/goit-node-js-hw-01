18dbad98fb2e9aeaf02e3a633e607e6280727125
Slava Pravda <pravdaslava7@gmail.com>
2022-10-21 20:16:32 +0300

    add hw

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
-const date = new Date();
+const { Command } = require("commander");
+const contacts = require("./contacts");
 
-console.log(`Current year ${date.getFullYear()}`);
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const contactsList = await contacts.listContacts();
+      console.log(contactsList);
+      break;
+
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      console.log(oneContact);
+      break;
+
+    case "add":
+      const add = await contacts.addContact({ name, email, phone });
+      console.log(add);
+      break;
+
+    case "remove":
+      const remove = await contacts.removeContact(id);
+      console.log(remove);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
